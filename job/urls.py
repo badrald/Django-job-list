af7583b610339416189adaac2b8e8a0e32fcf6cd
badr_alden <badrgta51@gamil.com>
2023-04-07 14:22:55 +0200

    add vewis & Urls

--- job/urls.py
+++ job/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from . import views
 
 urlpatterns = [
-    
+    path('',views.job_list,name='job_list'),
+    path('<int:id>/',views.job_detail,name='job_detail'),
 ]
